954d2edc22d7ab7fe8449a799252d727ca6878d7
sarah <sarah@triggersandsparks.com>
2017-01-21 00:49:31 +0000

    Start implementing some basic styles.

--- js/charts.js
+++ js/charts.js
@@ -1,19 +1,30 @@
 window.onload = function() {
 	var chart = c3.generate({
-	bindto: '#ethnicityChart',
-	data: {
-		columns: [
-			['White', 300],
-			['Latinx/Hispanic', 47],
-			['Black', 12],
-			['Asian', 29],
-			['Multiracial', 10],
-			['Other/prefer not to say', 18]
-		],
-		type: 'donut',
-	},
-	donut: {
-		title: "Ethnicity"
-	}
-});
+		bindto: '#ethnicityChart',
+		data: {
+			columns: [
+				['White', 300],
+				['Latinx/Hispanic', 47],
+				['Black', 12],
+				['Asian', 29],
+				['Multiracial', 10],
+				['Other/prefer not to say', 18]
+			],
+			type: 'donut',
+		},
+		donut: {
+			title: "Ethnicity",
+			width: 80
+		},
+		legend: {
+			position: 'right'
+		},
+		tooltip: {
+			format: {
+				value: function (value, ratio, id) {
+					return value;
+				}
+			}
+		}
+	});
 };
