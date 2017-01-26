091005f526a1a371ceb5e869afeed7bf77c7a158
sarah <sarah@triggersandsparks.com>
2017-01-26 12:28:49 +0000

    Add jQuery to add CSS class to active filter links.

--- js/charts.js
+++ js/charts.js
@@ -1,54 +1,52 @@
-window.onload = function() {
-	var ethnicityChart = c3.generate({
-		bindto: '#ethnicityChart',
-		data: {
-			url: 'data/ethnicity.json',
-			mimeType: 'json',
-			type: 'donut'
-		},
-		donut: {
-			title: "Ethnicity",
-			width: 80
-		},
-		legend: {
-			position: 'right'
-		},
-		tooltip: {
-			format: {
-				value: function (value, ratio, id) {
-					return value;
+(function( $ ){
+
+	window.onload = function() {
+		var ethnicityChart = c3.generate({
+			bindto: '#ethnicityChart',
+			data: {
+				url: 'data/ethnicity.json',
+				mimeType: 'json',
+				type: 'donut'
+			},
+			donut: {
+				title: "Ethnicity",
+				width: 80
+			},
+			legend: {
+				position: 'right'
+			},
+			tooltip: {
+				format: {
+					value: function (value, ratio, id) {
+						return value;
+					}
 				}
 			}
+		});
+
+		// Load new chart data.
+		function loadNewChartData(event) {
+			var filterLink = event.currentTarget;
+			$(filterLink).siblings().removeClass( 'active' );
+			$(filterLink).addClass( 'active' );
+			var source = event.currentTarget.dataset.source;
+			setTimeout(function() {
+				ethnicityChart.load({
+					//unload: true,
+					url: 'data/' + source + '.json',
+					mimeType: 'json'
+				});
+			}, 500);
 		}
-	});
 
 
-	function loadNewChartData(event) {
-		var source = event.currentTarget.dataset.source;
-		setTimeout(function() {
-			ethnicityChart.load({
-				//unload: true,
-				url: 'data/' + source + '.json',
-				mimeType: 'json'
-			});
-		}, 500);
-	}
+		// Get an array of our filter links.
+		var filters = document.getElementsByClassName( 'filter' );
 
-	var filters = document.getElementsByClassName( 'filter' );
-
-	for (var i = 0; i < filters.length; i++) {
-		// one way
-		filters[i].addEventListener('click', loadNewChartData, false);
-	}
-
-
-// add event listener to element
-
-	/*
-	filter.onclick = function() {
-
+		// For each filter, add an event listener to load new chart data.
+		for (var i = 0; i < filters.length; i++) {
+			filters[i].addEventListener('click', loadNewChartData, false);
+		}
 	};
-	*/
 
-
-};
+})( jQuery );
